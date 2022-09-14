6de97a3d36c7ed5d333f863507937864a5cbcebe
Janine Leal <janine.sleal@mercadolivre.com>
2022-09-14 09:31:36 -0300

    product service

---
+++ job-readiness/Models/ProductResponse.swift
@@ -0,0 +1,45 @@
+//
+//  ProductResponse.swift
+//  job-readiness
+//
+//  Created by Janine Silva Leal on 14/09/22.
+//
+
+import Foundation
+
+// MARK: - ProductResponse
+struct ProductResponse: Codable {
+    let code: Int?
+    let body: Body?
+}
+
+// MARK: - Body
+struct Body: Codable {
+    let title: String?
+    let officialStoreID: Int?
+    let price: Double?
+    let originalPrice: Double?
+    let currencyID: String?
+    let initialQuantity, availableQuantity, soldQuantity: Int?
+    let buyingMode, listingTypeID, startTime, stopTime: String?
+    let condition: String?
+    let permalink: String?
+    let thumbnailID: String?
+    let thumbnail: String?
+    let secureThumbnail: String?
+    let pictures: [Picture]?
+    let videoID: String?
+    let acceptsMercadopago: Bool?
+    let status: String?
+    let tags: [String]?
+    let warranty: String?
+    let dateCreated, lastUpdated: String?
+}
+
+// MARK: - Picture
+struct Picture: Codable {
+    let id: String?
+    let url: String?
+    let secureURL: String?
+    let size, maxSize, quality: String?
+}
--- job-readiness/Service/SearchService.swift
+++ job-readiness/Service/SearchService.swift
@@ -29,11 +29,28 @@ class SearchService {
     }
     
     func getProductsIDs(token: String, categoryID: String, completion: @escaping (ProductByCategoryResponse?) -> Void) {
-        let urlExtension = "/highlights/MLM/category/\(categoryID)"
+        let urlExtension = "/highlights/MLB/category/\(categoryID)"
         
         apiClient.get(token: token, urlExtension: urlExtension) { data, error in
             do {
                 let response = try JSONDecoder().decode(ProductByCategoryResponse.self, from: data)
+                completion(response)
+            } catch let error {
+                print("error: \(error)")
+            }
+        }
+    }
+    
+    func getProducts(token: String, itemsIds: [String], completion: @escaping (ProductResponse?) -> Void) {
+        let urlExtension = " "
+        
+        let params = [
+            "ids" : itemsIds.joined(separator: ",")
+        ]
+        
+        apiClient.get(token: token, parameters: params, urlExtension: urlExtension) { data, error in
+            do {
+                let response = try JSONDecoder().decode(ProductResponse.self, from: data)
                 completion(response)
             } catch let error {
                 print("error: \(error)")
